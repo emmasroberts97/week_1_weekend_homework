642d7257b8931debc4e5645e241fcb7389014bde
Emma Roberts <emma.roberts197@gmail.com>
2020-01-10 13:57:37 +0000

    Weekend Homework

---
+++ pet_shop.rb
@@ -0,0 +1,106 @@
+def pet_shop_name(pet_shop)
+  return pet_shop[:name]
+end
+
+def total_cash(pet_shop)
+  return pet_shop[:admin][:total_cash]
+end
+
+def add_or_remove_cash(pet_shop, amount)
+  if amount > 0
+  pet_shop[:admin][:total_cash] += amount
+  elsif amount < 0
+  amount = amount * -1
+  pet_shop[:admin][:total_cash] -= amount
+  end
+end
+
+def pets_sold(pet_shop)
+  return pet_shop[:admin][:pets_sold]
+end
+
+def increase_pets_sold(pet_shop, amount)
+  pet_shop[:admin][:pets_sold] += amount
+end
+
+def stock_count(pet_shop)
+  pet_shop[:pets].count
+end
+
+def pets_by_breed(pet_shop, breed)
+  pets_name = []
+  for animal in pet_shop[:pets]
+    if animal[:breed] == breed
+      pets_name.push(animal)
+    end
+  end
+  return pets_name
+end
+
+def find_pet_by_name(pet_shop, name)
+  for pet in pet_shop[:pets]
+    if pet[:name] == name
+        return pet
+    end
+  end
+  return nil
+end
+
+def remove_pet_by_name(pet_shop, name)
+  for pet in pet_shop[:pets]
+    if pet[:name] == name
+      pet_shop[:pets].delete(pet)
+    end
+  end
+end
+
+def add_pet_to_stock(pet_shop, new_pet)
+  pet_shop[:pets].push(new_pet)
+end
+
+def customer_cash(customer)
+  return customer[:cash]
+end
+
+def remove_customer_cash(customer, amount)
+  return customer[:cash] -= amount
+end
+
+def customer_pet_count(customer)
+  return customer[:pets].count
+end
+
+def add_pet_to_customer(customer, new_pet)
+  customer[:pets].push(new_pet)
+end
+
+def customer_can_afford_pet(customer, pet)
+  if customer[:cash] >= pet[:price]
+    return true
+  else
+    return false
+  end
+end
+
+def sell_pet_to_customer(pet_shop, pet, customer)
+  if pet != nil
+    if customer_can_afford_pet(customer, pet) == true
+      sell_pet(customer, pet)
+      add_pet_price_to_pet_shop(pet_shop, pet)
+      add_pet_to_pets_sold(pet_shop)
+      add_pet_to_customer(customer, pet)
+    end
+  end
+end
+
+def sell_pet(customer, pet)
+  customer[:cash] -= pet[:price]
+end
+
+def add_pet_price_to_pet_shop(pet_shop, pet)
+  pet_shop[:admin][:total_cash] += pet[:price]
+end
+
+def add_pet_to_pets_sold(pet_shop)
+  pet_shop[:admin][:pets_sold] += 1
+end
